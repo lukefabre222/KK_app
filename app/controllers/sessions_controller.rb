01150697ed43b11659c011cd1fbfc017bd10ff44
EC2 Default User <ec2-user@ip-172-31-29-42.us-east-2.compute.internal>
2020-02-29 15:37:17 +0000

    basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+  
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      flash[:success] ="ログインに成功しました"
+      redirect_to user
+    else
+      flash.now[:danger] ="メールアドレス、又はパスワードが間違っています"
+      render "new"
+    end
+  end
+  
+  def destroy
+    log_out
+    flash[:danger] ="ログアウトしました"
+    redirect_to root_path
+  end
+  
+end
