8766b7fb814ae3e81ae1cbfbd4e3a8d7afefeff0
EC2 Default User <ec2-user@ip-172-31-29-42.us-east-2.compute.internal>
2020-03-14 17:38:55 +0000

    finished besic app

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,30 +1,59 @@
 class AttendancesController < ApplicationController
   
   def new
-    @user = User.find(params[:user_id])
-    @attendance = Attendance.new
+    @user = User.find(params[:id])
+    @attendance = @user.attendances.find_by(worked_on: Date.today)
+    @first_day = first_day(params[:date])
+    @last_day = @first_day.end_of_month
+    @dates = user_attendances_month_date
   end
   
-def create
-  @user = User.find(params[:user_id])
-  @attendance = @user.attendances.find_by(worked_on: Date.today)
-  if @attendance.started_at.nil?
-    @attendance.update_attributes(started_at: current_time)
-    flash[:info] = 'おはようございます。'
-  elsif @attendance.finished_at.nil?
-    @attendance.update_attributes(finished_at: current_time)
-    flash[:info] = 'おつかれさまでした。'
-  else
-    flash[:danger] = 'トラブルがあり、登録出来ませんでした。'
-  end
-  redirect_to @user
-end
-  
-private
-
-    def attendance_params
-      params.permit(:worked_on, :started_at, :finished_at, :note)
+  def index
+    @user = User.find(params[:id])
+    @first_day = first_day(params[:first_day])
+    @last_day = @first_day.end_of_month
+    (@first_day..@last_day).each do |day|
+      unless @user.attendances.any? {|attendance| attendance.worked_on == day}
+        record = @user.attendances.build(worked_on: day)
+        record.save
+      end
     end
+    @dates = user_attendances_month_date
+    @worked_sum = @dates.where.not(started_at: nil).count
+  end
   
-
+  
+  def edit
+    @user = User.find(params[:id])
+    @first_day = first_day(params[:date])
+    @last_day = @first_day.end_of_month
+    @dates = user_attendances_month_date
+  end
+  
+  def create
+    @user = User.find(params[:id])
+    @attendance = @user.attendances.find_by(worked_on: Date.today)
+    if @attendance.update_attributes(started_at:params[:started_at], finished_at:params[:finished_at], note:params[:note])
+      @attendance.save
+      redirect_to @user
+    else
+      render new_attendances_path
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    attendances_params.each do |id, item|
+      attendance = Attendance.find(id)
+      attendance.update_attributes(item)
+    end
+    flash[:success] = '勤怠情報を更新しました。'
+    redirect_to user_attendances_path
+  end
+    
+  private
+  
+    def attendances_params
+      params.permit(attendances: [:started_at, :finished_at, :note])[:attendances]
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   get '/edit-basic-info/:id', to: 'users#edit_basic_info', as: :basic_info
   patch '/update_basic_info', to: 'users#update_basic_info'
-  post '/users/:id/attendances/new', to: 'attendances#create'
-  
+  get 'users/:id/attendances/:date/edit', to: 'attendances#edit', as: :edit_attendances
+  patch 'users/:id/attendances/:date/update', to: 'attendances#update', as: :update_attendances
+  get 'users/:id/attendances', to: 'attendances#index', as: :user_attendances
   resources :users do
     resources :attendances
   end
